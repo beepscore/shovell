8e313289651dd4c156aaba43a52b2c987d8823b3
Steve Baker <support@beepscore.com>
2009-05-13 16:53:24 -0700

    Ch 06 pg 162
    Added new method to stories_controller.
    Going to stories/new throws NoMethodError

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,8 @@
 class StoriesController < ApplicationController
   def index
-  	@current_time = Time.now
   	@story = Story.find(:first, :order => 'RANDOM()')
+  end
+  def new
+  	@story = Story.new
   end
 end
