06b25def37f0a04c5379e1724bcfac04a1a2dc68
beepscore <support@beepscore.com>
2009-05-24 23:38:53 -0700

    Ch 06 pg 174 Added flash notice.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,6 +8,7 @@ class StoriesController < ApplicationController
     def create
   	@story = Story.new(params[:story])
   	@story.save
+    flash[:notice] = 'Story submission succeeded'
   	redirect_to stories_path
   end
 end
