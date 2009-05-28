dd80ff9d6641ac7f9d65672433d2adba6a5471de
beepscore <support@beepscore.com>
2009-05-28 15:24:27 -0700

    Ch 06 pg 193  Added more functional tests

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -7,4 +7,31 @@ class StoriesControllerTest < ActionController::TestCase
     assert_template 'index'
     assert_not_nil assigns(:story)
   end
+
+  def test_should_show_new
+    get :new
+    assert_response :success
+    assert_template 'new'
+    assert_not_nil assigns(:story)
+  end
+
+  def test_should_show_new_form
+    get :new
+    assert_select 'form p', :count => 3
+  end
+
+  def test_should_add_story
+    post :create, :story => {
+        :name => 'test story',
+        :link => 'http://www.test.com/'
+    }
+    assert ! assigns(:story).new_record?
+    assert_redirected_to stories_path
+    assert_not_nil flash[:notice]
+  end
+
+  def test_should_reject_missing_story_attribute
+    post :create, :story => { :name => 'story without a link' }
+    assert assigns(:story).errors.on(:link)
+  end
 end
