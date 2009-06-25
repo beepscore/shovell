1f4af7e6b6879170f2d52ceda69e69286fe1e400
Steve Baker <support@beepscore.com>
2009-06-25 13:48:05 -0700

    Ch 07 pg 229  Add respond_to request format html or javascript Tested changes by voting for a story.  This wrote to the database.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,11 @@ class VotesController < ApplicationController
   def create
     @story = Story.find(params[:story_id])
     @story.votes.create
+
+    respond_to do |format|
+      format.html { redirect_to @story }
+      format.js
+    end
   end
 
 end
