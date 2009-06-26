35fb534c1b9ccbf9c8d56383eb6eba8c4990cc49
Steve Baker <support@beepscore.com>
2009-06-25 20:51:20 -0700

    Ch 07 pg 237 call partial in create.js.rjs, add 'latest' method Update list when shove it vote button is clicked. Limit list of votes to 3, sort descending.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,10 @@
 class Story < ActiveRecord::Base
   validates_presence_of :name, :link
-  has_many :votes
+  has_many :votes do
+    def latest
+      find :all, :order => 'id DESC', :limit => 3
+    end
+  end
   def to_param
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
