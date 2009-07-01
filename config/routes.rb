3a1b62d1e88a7e5f7958fb97b23e2b2f7332a067
Steve Baker <support@beepscore.com>
2009-06-30 17:25:54 -0700

    Ch08 pg260 Generated controller Sessions, added session resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :stories, :has_many => :votes
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.resource :session
 end
