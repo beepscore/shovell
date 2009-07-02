ed9d48c71da19d52e741366ddf5a770701e6b4d6
Steve Baker <support@beepscore.com>
2009-07-02 00:00:29 -0700

    Ch08 pg265 Views sessions new add form, controller implement create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @current_user = User.find_by_login_and_password(
+        params[:login], params[:password])
+
+    if @current_user
+      session[:user_id] = @current_user.id
+      redirect_to stories_path
+    else
+      render :action => 'new'
+    end
   end
 
   def destroy
