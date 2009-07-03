85dc4405114e33fbbb54eac49315c1d9b379c139
Steve Baker <support@beepscore.com>
2009-07-02 22:39:23 -0700

    Ch08 pg281 User must log in to create a story.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,5 @@
 class StoriesController < ApplicationController
+  before_filter :login_required, :only => [ :new, :create ]
   def index
     @story = Story.find(:first, :order => 'RANDOM()')
   end
